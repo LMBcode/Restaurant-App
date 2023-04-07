550c604e7900eaf7333815506a7b7cbbd1814ee0
Mombacke <mouhamed@ezfirecontrols.com>
2023-04-07 19:08:18 +0200

    detail layout

---
+++ lib/pages/food_detail.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+import '../foods_list.dart';
+
+class FoodDetails extends StatefulWidget {
+  final MenuItem menuItem;
+
+  const FoodDetails({super.key, required this.menuItem});
+
+  @override
+  _FoodDetailsState createState() => _FoodDetailsState();
+}
+
+class _FoodDetailsState extends State<FoodDetails> {
+  int quantity = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+      ),
+      body: part(widget.menuItem, context),
+    );
+  }
+
+  Widget part(MenuItem menuItem, BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Image.asset(
+          menuItem.image,
+          height: size.height * 0.25,
+          width: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30))),
+            child: Column(
+              children: [
+                Text(menuItem.name,
+                    style: const TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.normal)),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.timer,
+                            size: 18,
+                          ),
+                          Text(
+                            "${menuItem.preparationTime} min",
+                            style: const TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.normal),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.local_fire_department,
+                              size: 18,
+                            ),
+                            Text(
+                              "${menuItem.kcal} kcal",
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          right: 50), // Add extra padding to the right
+                      child: Stack(clipBehavior: Clip.none, children: [
+                        Container(
+                          width: 100,
+                          padding: const EdgeInsets.all(8),
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30))),
+                                  child: Text(
+                              "\$${menuItem.price}",
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.normal),
+                            ),
+                        ),
+                        Positioned(
+                          left: 60,
+                          child: Container(
+                            width: 110,
+                            padding: const EdgeInsets.all(8),
+                            decoration: const BoxDecoration(
+                                color: Colors.red,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30))),
+                              
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                    const Icon(Icons.remove),
+
+                                    Container(
+                                      width: 25,
+                                      height: 25,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(Radius.circular(50))
+                                      ),
+                                      child: const Center(child: Text("1")),
+                                    ),
+                                    const Icon(
+                                      Icons.add,                                                         
+                                      ),
+
+
+                              ]),
+            
+                          ),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
